5723cf9d86a16e2443baf9c6c9c3c7aa9b0c6355
lothrazar <samson.bassett@gmail.com>
2023-08-26 10:47:05 -0700

    non dynamic reg

--- src/main/java/com/lothrazar/veincreeper/ModMain.java
+++ src/main/java/com/lothrazar/veincreeper/ModMain.java
@@ -1,12 +1,11 @@
 package com.lothrazar.veincreeper;
 
-import net.minecraftforge.eventbus.api.IEventBus;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @Mod(ModMain.MODID)
 public class ModMain {
@@ -15,10 +14,10 @@ public class ModMain {
   public static final Logger LOGGER = LogManager.getLogger();
 
   public ModMain() {
-    IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
-    ModRegistry.BLOCKS.register(eventBus);
-    ModRegistry.ITEMS.register(eventBus);
-    ModRegistry.ENTITIES.register(eventBus);
+    //    IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+    //    ModRegistry.BLOCKS.register(eventBus);
+    //    ModRegistry.ITEMS.register(eventBus);
+    //    ModRegistry.ENTITIES.register(eventBus);
     ConfigManager.setup();
     FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
     FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupClient);
--- src/main/java/com/lothrazar/veincreeper/ModRegistry.java
+++ src/main/java/com/lothrazar/veincreeper/ModRegistry.java
@@ -1,45 +1,48 @@
 package com.lothrazar.veincreeper;
 
-import net.minecraft.world.entity.Entity;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegistryObject;
+import net.minecraftforge.registries.RegisterEvent;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ModRegistry {
 
   public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ModMain.MODID);
   public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, ModMain.MODID);
+  //  public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, ModMain.MODID);
+  static EntityType.Builder BUILDER = EntityType.Builder.<PartyCreeper> of(PartyCreeper::new, MobCategory.MISC).sized(1.4F, 2.7F - 0.3F).clientTrackingRange(10);
+  //  public static final RegistryObject<EntityType<PartyCreeper>> CREEP = ENTITIES.register("party", () ->
+  //  //BUILDER
+  //  BUILDER.build("party"));
+  static EntityType party;
 
-  //
-
-  public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, ModMain.MODID);
-
-  public static final RegistryObject<EntityType<PartyCreeper>> CREEP = ENTITIES.register("party", () -> register("party",
-      EntityType.Builder.<PartyCreeper> of(PartyCreeper::new, MobCategory.MISC).sized(1.4F, 2.7F-0.3F).clientTrackingRange(10)));
-
-//helper
-  public static <T extends Entity> EntityType<T> register(String id, EntityType.Builder<T> builder) {
-    return builder.build(id);
+  @SubscribeEvent
+  public static void onRegistry(RegisterEvent event) {
+    event.register(Registries.ENTITY_TYPE, reg -> {
+      //
+      party = BUILDER.build("party");
+      reg.register("party", party);
+      //
+      //
+    });
   }
-
 
   @SubscribeEvent
   public static void onEntityAttributeCreationEvent(EntityAttributeCreationEvent event) {
-    event.put(CREEP.get(), PartyCreeper.createAttributes().build());
+    event.put(party, PartyCreeper.createAttributes().build());
   }
 
   @SubscribeEvent
   public static void entityRenderers(EntityRenderersEvent.RegisterRenderers event) {
-    event.registerEntityRenderer(CREEP.get(), PartyCreeperRender::new);
+    event.registerEntityRenderer(party, PartyCreeperRender::new);
   }
 }
