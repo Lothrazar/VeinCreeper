2b3685b9a30df4c8c76c3a01889b60eddb921dc7
lothrazar <samson.bassett@gmail.com>
2023-08-27 16:18:03 -0700

    zenscript bonuses

--- src/main/java/com/lothrazar/veincreeper/recipe/CreeperZen.java
+++ src/main/java/com/lothrazar/veincreeper/recipe/CreeperZen.java
@@ -36,6 +36,19 @@ public class CreeperZen implements IRecipeManager<ExplosionRecipe> {
   }
 
   @ZenCodeType.Method
+  public void addRecipe(String name, String entityType, String target, String blockId, String bonusId, float chance) {
+    name = fixRecipeName(name);
+    TagKey<Block> targetMe = TagKey.create(Registries.BLOCK, new ResourceLocation(target));
+    Block ore = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(blockId));
+    Block bonus = ForgeRegistries.BLOCKS.getValue(new ResourceLocation(bonusId));
+    ExplosionRecipe m = new ExplosionRecipe(new ResourceLocation("crafttweaker", name),
+        targetMe,
+        ore, entityType, bonus, chance);
+    CraftTweakerAPI.apply(new ActionAddRecipe<ExplosionRecipe>(this, m, ""));
+    VeinCreeperMod.LOGGER.info("crafttweaker: Recipe loaded " + m.getId().toString());
+  }
+
+  @ZenCodeType.Method
   public void removeRecipe(String... names) {
     removeByName(names);
     VeinCreeperMod.LOGGER.info("crafttweaker: Recipe removed " + names);
